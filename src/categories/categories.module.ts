f862158a6148248198d8927b9bc92f5967357077
ezla <garylopezdavila8@gmail.com>
2026-05-03 20:13:57 -0600

    fix: register entity metadata for relations

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
 
 @Module({
+  // // Registra la entidad Category en el contexto de este módulo
+  imports: [TypeOrmModule.forFeature([Category])], 
   controllers: [CategoriesController],
   providers: [CategoriesService],
+  // // Exporta TypeOrmModule para que otros módulos (como Products) vean esta entidad
+  exports: [TypeOrmModule] 
 })
 export class CategoriesModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { Product } from './entities/product.entity'; 
+import { Product } from './entities/product.entity';
+import { Category } from '../categories/entities/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])], 
+  // // Registra ambas entidades para habilitar la relación ManyToOne / OneToMany
+  imports: [TypeOrmModule.forFeature([Product, Category])], 
   controllers: [ProductsController],
   providers: [ProductsService],
+  // // Exporta el servicio para uso externo si es necesario
   exports: [ProductsService],
 })
 export class ProductsModule {}
